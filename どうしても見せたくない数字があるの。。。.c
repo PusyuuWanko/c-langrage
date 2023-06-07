1ac535abe39b99bfcaf68149c13766e860270a45
Pusyuu <121094209+PusyuuOfficial@users.noreply.github.com>
2023-06-07 10:36:27 +0900

    Add files via upload

---
+++ どうしても見せたくない数字があるの。。。.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main (void) {
+  int suuti;
+  scanf("%d", &suuti);
+  if (suuti != 24)
+  printf("miseteiikedo...");
+  return 0;
+}
---
+++ 代入された数値が256さんかどうかの真偽.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main (void) {
+  int suuti;
+  scanf("%d", &suuti);
+  if (suuti == 256)
+  printf("256sannyoriookiidesune.");
+  return 0;
+}
