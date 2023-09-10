b900d1f5eba393c0b0ca80c55c9ef1c9807098af
プシューわんこ/ <121094209+PusyuuWanko@users.noreply.github.com>
2023-09-10 23:46:44 +0900

    add

---
+++ bmi計算機.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main() {
+  setlocale(LC_ALL, "ja_JP.UTF-8");
+
+  double weight, height, bmi;
+
+  printf("体重（kg）を入力してください：");
+  scanf("%lf", &weight);
+
+  printf("身長（m）を入力してください：");
+  scanf("%lf", &height);
+
+  bmi = weight / (height * height);
+
+  printf("BMIは %.2lf です。\n", bmi);
+
+  return 0;
+}
